d84b4dfa3f3a950af87b1e633b2b2a6d81eedc46
Karatug Ozan Bircan <karatugo@gmail.com>
2020-06-28 21:42:40 +0300

    React app is up and running
    - Tried some Rebass components to make sure that it is running
    - Upgrade node version

--- leadmanager/frontend/src/components/App.js
+++ leadmanager/frontend/src/components/App.js
@@ -1,10 +1,46 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
+import {
+    Box,
+    Card,
+    Image,
+    Heading,
+    Text,
+    Flex
+} from 'rebass'
+
+
 
 class App extends Component {
     render() {
-        return <h1>React App</h1>
+        return (
+            <div>
+                <Heading
+                    fontSize={[5, 6, 7]}
+                    color='primary'>
+                    Hello
+                    </Heading>
+                <Flex>
+                    <Box
+                        p={3}
+                        width={1 / 2}
+                        color='white'
+                        bg='primary'>
+                        Flex
+            </Box>
+                    <Box
+                        p={3}
+                        width={1 / 2}
+                        color='white'
+                        bg='secondary'>
+                        Box
+            </Box>
+                </Flex></div>
+
+        )
     }
 }
 
 ReactDOM.render(<App />, document.getElementById('app'))
+
+
